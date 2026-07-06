a57a8202976fdbde135620e393803d1769ba3f6d
장원준 <wonjun001227@gmail.com>
2026-07-06 22:28:00 +0900

    chore: Next.js 프로젝트 설정 (next.config.ts)
    - 외부 이미지 허용 도메인 등록 (Supabase, YouTube, 뉴스 언론사)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,51 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'shphrkjoatkjbbxxdfih.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.bbc.co.uk',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.autosport.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.motorsport.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.racefans.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.the-race.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.crash.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.gpfans.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.motorsportweek.com',
+      },
+    ],
+  },
+  poweredByHeader: false,
 };
 
 export default nextConfig;
