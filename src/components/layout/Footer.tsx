f62610d21caba4aa6cffd618601f378487fd5925
장원준 <wonjun001227@gmail.com>
2026-08-11 20:33:22 +0900

    fix: Footer 인스타 아이콘 img 경고 억제 주석 추가
    - src/components/layout/Footer.tsx: 인스타그램 아이콘 img 태그에 eslint-disable-next-line 주석 추가, 프로젝트 전반의 고정 크기 아이콘 관례와 통일해 @next/next/no-img-element 경고 제거

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -15,6 +15,7 @@ export default function Footer() {
               rel="noopener noreferrer"
               aria-label="WhatisF1 Instagram"
             >
+              {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
                 src="/images/common/instagram_logo.png"
                 alt="인스타그램"
